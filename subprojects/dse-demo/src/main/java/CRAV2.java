b83956f9742a57e971a65abd0d4e388b1bc13e95
Fozail Ahmad <fozail.ahmad@mail.mcgill.ca>
2023-09-27 18:21:33 -0400

    Updated CRA DSE

--- subprojects/dse-demo/src/main/java/CRAV2.java
+++ subprojects/dse-demo/src/main/java/CRAV2.java
@@ -1,47 +1,72 @@
-import tools.refinery.store.query.dnf.*;
+import tools.refinery.store.dse.strategy.BestFirstStoreManager;
+import tools.refinery.store.dse.transition.Rule;
+import tools.refinery.store.dse.transition.actions.ActionLiteral;
+import tools.refinery.store.dse.transition.actions.ActionLiterals;
+import tools.refinery.store.dse.transition.objectives.Criteria;
+import tools.refinery.store.dse.transition.objectives.Objectives;
+import tools.refinery.store.model.ModelStore;
+import tools.refinery.store.query.ModelQueryAdapter;
+import tools.refinery.store.query.dnf.Dnf;
+import tools.refinery.store.query.dnf.FunctionalQuery;
+import tools.refinery.store.query.dnf.Query;
+import tools.refinery.store.query.dnf.RelationalQuery;
 import tools.refinery.store.query.term.Variable;
 import tools.refinery.store.query.term.real.RealTerms;
-import tools.refinery.store.query.interpreter.QueryInterpreterAdapter;
 import tools.refinery.store.query.view.KeyOnlyView;
 import tools.refinery.store.representation.Symbol;
-import tools.refinery.store.model.ModelStore;
-import tools.refinery.store.query.dnf.RelationalQuery;
+import tools.refinery.store.query.interpreter.QueryInterpreterAdapter;
+import tools.refinery.store.statecoding.StateCoderAdapter;
+import tools.refinery.store.dse.modification.ModificationAdapter;
+import tools.refinery.store.dse.transition.DesignSpaceExplorationAdapter;
 
+import java.util.List;
+
+import static tools.refinery.store.dse.transition.actions.ActionLiterals.add;
+import static tools.refinery.store.dse.transition.actions.ActionLiterals.remove;
+import static tools.refinery.store.dse.modification.actions.ModificationActionLiterals.create;
+import static tools.refinery.store.dse.modification.actions.ModificationActionLiterals.delete;
 import static tools.refinery.store.query.literal.Literals.check;
 import static tools.refinery.store.query.literal.Literals.not;
 import static tools.refinery.store.query.term.int_.IntTerms.constant;
 import static tools.refinery.store.query.term.int_.IntTerms.greater;
 import static tools.refinery.store.query.term.int_.IntTerms.sub;
 import static tools.refinery.store.query.term.real.RealTerms.*;
-import static tools.refinery.store.query.term.real.RealTerms.asReal;
+import static tools.refinery.store.query.term.real.RealTerms.add;
 
-public class CRAStore {
+import tools.refinery.store.dse.modification.DanglingEdges;
+import tools.refinery.store.tuple.Tuple;
+import tools.refinery.visualization.ModelVisualizerAdapter;
+import tools.refinery.visualization.internal.FileFormat;
+
+public class CRAV2 {
+
+	// Symbols
 	private final Symbol<String> name = new Symbol<>("name", 1, String.class, null);
 	private final Symbol<Boolean> attribute = new Symbol<>("attribute", 1, Boolean.class, false);
 	private final Symbol<Boolean> method = new Symbol<>("method", 1, Boolean.class, false);
 	private final Symbol<Boolean> class_ = new Symbol<>("class", 1, Boolean.class, false);
-	private final Symbol<Integer> nextID = new Symbol<>("nextID", 0, Integer.class, 0);
 
 	private final Symbol<Boolean> functionalDependency = new Symbol<>("functionalDependency", 2, Boolean.class, false);
 	private final Symbol<Boolean> dataDependency = new Symbol<>("dataDependency", 2, Boolean.class, false);
 	private final Symbol<Boolean> isEncapsulatedBy = new Symbol<>("isEncapsulatedBy", 2, Boolean.class, false);
 
-	// *** What is a Relational Query
+
+	// Queries
 	private final RelationalQuery classes;
 	private final RelationalQuery features;
 	private final RelationalQuery nonEncapsulatedFeatures;
 	private final RelationalQuery methodsForClass;
 	private final RelationalQuery attributesForClass;
-	private final RelationalQuery deleteClassPrecondition;
-	private final RelationalQuery createClassPrecondition;
-	private final RelationalQuery moveFeaturePrecondition;
-	private final RelationalQuery assignFeaturePrecondition;
+	private final Rule deleteClass;
+	private final Rule createClass;
+	private final Rule moveFeature;
+	private final Rule assignFeature;
 	private final FunctionalQuery<Double> NormalizedMAI;
 	private final FunctionalQuery<Double> NormalizedMMI;
 	private final FunctionalQuery<Double> CRA;
-	private final ModelStore store;
 
-	public CRAStore() {
+
+	public CRAV2() {
 		var attributeView = new KeyOnlyView<>(attribute);
 		var methodView = new KeyOnlyView<>(method);
 		var classView = new KeyOnlyView<>(class_);
@@ -71,7 +96,7 @@ public class CRAStore {
 		var class2 = Variable.of("class2");
 		nonEncapsulatedFeatures = Query.builder("NonEncapsulatedFeatures")
 				.parameters(feature2)
-				.clause(features.call(feature2), not(isEncapsulatedByView.call(feature2,class2)))
+				.clause(features.call(feature2), not(isEncapsulatedByView.call(feature2, class2)))
 				.build();
 
 		// Return a table with all the methods for each given class
@@ -79,7 +104,7 @@ public class CRAStore {
 		var associated_method = Variable.of("associated_method");
 		methodsForClass = Query.builder("methodsForClass")
 				.parameters(given_class_for_method, associated_method)
-				.clause(isEncapsulatedByView.call(associated_method,given_class_for_method),
+				.clause(isEncapsulatedByView.call(associated_method, given_class_for_method),
 						methodView.call(associated_method))
 				.build();
 
@@ -88,7 +113,7 @@ public class CRAStore {
 		var associated_attribute = Variable.of("associated_attribute");
 		attributesForClass = Query.builder("attributesForClass")
 				.parameters(given_class_for_attribute, associated_attribute)
-				.clause(isEncapsulatedByView.call(associated_attribute,given_class_for_attribute),
+				.clause(isEncapsulatedByView.call(associated_attribute, given_class_for_attribute),
 						attributeView.call(associated_attribute))
 				.build();
 
@@ -99,7 +124,7 @@ public class CRAStore {
 		var attribute_1_mai = Variable.of("attribute_1_mai");
 		var classDataDependencies = Dnf.builder("ClassDataDependencies")
 				.parameters(class_1_mai, class_2_mai, method_1_mai, attribute_1_mai)
-				.clause(classView.call(class_1_mai), methodView.call(method_1_mai), isEncapsulatedByView.call(method_1_mai,class_1_mai), classView.call(class_2_mai),
+				.clause(classView.call(class_1_mai), methodView.call(method_1_mai), isEncapsulatedByView.call(method_1_mai, class_1_mai), classView.call(class_2_mai),
 						attributeView.call(attribute_1_mai), isEncapsulatedByView.call(attribute_1_mai, class_2_mai), dataDependencyView.call(method_1_mai, attribute_1_mai))
 				.build();
 
@@ -114,7 +139,7 @@ public class CRAStore {
 		NormalizedMAI = Query.builder("MAI")
 				.parameters(class_1_normalized_mai, class_2_normalized_mai)
 				.output(NormalizedMAIOutput)
-				.clause(classDataDependencies.call(class_1_normalized_mai,class_2_normalized_mai,
+				.clause(classDataDependencies.call(class_1_normalized_mai, class_2_normalized_mai,
 								method_1_normalized_mai, attribute_1_normalized_mai),
 						MAIOutput.assign(classDataDependencies.count(class_1_normalized_mai, class_2_normalized_mai,
 								Variable.of(), Variable.of())),
@@ -133,7 +158,7 @@ public class CRAStore {
 		var method_2_mmi = Variable.of("method_2_mmi");
 		var classFunctionalDependencies = Dnf.builder("ClassFunctionalDependencies")
 				.parameters(class_1_mmi, class_2_mmi, method_1_mmi, method_2_mmi)
-				.clause(classView.call(class_1_mmi), methodView.call(method_1_mmi),isEncapsulatedByView.call(method_1_mmi,
+				.clause(classView.call(class_1_mmi), methodView.call(method_1_mmi), isEncapsulatedByView.call(method_1_mmi,
 								class_1_mmi), classView.call(class_2_mmi), methodView.call(method_2_mmi),
 						isEncapsulatedByView.call(method_2_mmi, class_2_mmi),
 						functionalDependencyView.call(method_1_mmi, method_2_mmi))
@@ -156,7 +181,7 @@ public class CRAStore {
 		NormalizedMMI = Query.builder("MMI")
 				.parameters(class_1_normalized_mmi, class_2_normalized_mmi)
 				.output(NormalizedMMIOutput)
-				.clause(classFunctionalDependencies.call(class_1_normalized_mmi,class_2_normalized_mmi,
+				.clause(classFunctionalDependencies.call(class_1_normalized_mmi, class_2_normalized_mmi,
 								method_1_normalized_mmi, method_2_normalized_mmi),
 						MMIOutput.assign(classFunctionalDependencies.count(class_1_normalized_mmi,
 								class_2_normalized_mmi, Variable.of(), Variable.of())),
@@ -190,6 +215,8 @@ public class CRAStore {
 				.build();
 
 		// Depending on the Query we call, we need to pass in the right number of parameters
+		// *** WARNING ***//
+		// FOR OUR DSE FRAMEWORK WE ARE CALCULATION THE NEGATIVE CRA IN ORDER TO MINIMIZE IT INSTEAD OF MAX
 		var CRA_Index = Variable.of("CRA_Index", Double.class);
 		var Cohesion_MAI = Variable.of("Cohesion_MAI", Double.class);
 		var Cohesion_MMI = Variable.of("Cohesion_MMI", Double.class);
@@ -209,100 +236,169 @@ public class CRAStore {
 								given_class_cra_4)),
 						Coupling_MMI.assign(DifferentNormalizedMMI.aggregate(REAL_SUM, given_class_cra_5,
 								given_class_cra_6)),
-						CRA_Index.assign(RealTerms.sub(add(Cohesion_MAI, Cohesion_MMI), add(Coupling_MAI, Coupling_MMI))))
+						CRA_Index.assign(RealTerms.sub(add(Coupling_MAI, Coupling_MMI), add(Cohesion_MAI, Cohesion_MMI))))
 				.build();
 
 		// Created a Query that is the precondition for deleting a class
 		// Will return a table that contians two colums, one colum with classID, another with boolean existence
 		// Each row should contain a class does encapsulates no features
-		deleteClassPrecondition = Query.of("deleteClassPrecondition", (builder, classID) -> builder.clause(
-				classView.call(classID),
-				not(isEncapsulatedByView.call(Variable.of(), classID))
-		));
+		deleteClass = Rule.of("deleteClassPrecondition", (builder, classID) -> builder
+				.clause(
+						classView.call(classID),
+						not(isEncapsulatedByView.call(Variable.of(), classID)))
+				.action(
+						remove(class_, classID),
+						delete(classID, DanglingEdges.IGNORE)
+				));
 
-		createClassPrecondition = Query.of("createClassPrecondition", (builder, featureID) -> builder.clause(
-				features.call(featureID),
-				not(isEncapsulatedByView.call(featureID, Variable.of()))
-		));
-
-		moveFeaturePrecondition = Query.of("moveFeaturePrecondition", (builder, featureID, sourceClassID,
-				targetClassID) -> builder.clause(
+		createClass = Rule.of("createClassPrecondition", (builder, featureID) -> builder
+				.clause(
 						features.call(featureID),
-				isEncapsulatedByView.call(featureID, sourceClassID),
-				classView.call(targetClassID),
-				sourceClassID.notEquivalent(targetClassID)
-		));
+						not(isEncapsulatedByView.call(featureID, Variable.of())))
+				.action((newClassID) -> List.of(
+						create(newClassID),
+						add(class_, newClassID),
+						add(isEncapsulatedBy, featureID, newClassID)
+				)));
 
-		assignFeaturePrecondition = Query.of("assignFeaturePrecondition", (builder, featureID, targetClassID) ->
+		moveFeature = Rule.of("moveFeature",
+				(builder, featureID, sourceClassID, targetClassID) -> builder
+						.clause(
+								features.call(featureID),
+								isEncapsulatedByView.call(featureID, sourceClassID),
+								classView.call(targetClassID),
+								sourceClassID.notEquivalent(targetClassID)
+						)
+						.action(
+								remove(isEncapsulatedBy, featureID, sourceClassID),
+								add(isEncapsulatedBy, featureID, targetClassID)
+						));
+
+		assignFeature = Rule.of("assignFeaturePrecondition", (builder, featureID, targetClassID) ->
 				builder.clause(
 						nonEncapsulatedFeatures.call(featureID),
 						classView.call(targetClassID)
-				));
+						)
+						.action(
+								add(isEncapsulatedBy, featureID, targetClassID)
+						));
+	}
 
-		store = ModelStore.builder()
-				.symbols(name, attribute, method, class_, nextID, functionalDependency, dataDependency,
-						isEncapsulatedBy)
-				.with(QueryInterpreterAdapter.builder().queries(createClassPrecondition, deleteClassPrecondition,
-						moveFeaturePrecondition, assignFeaturePrecondition, nonEncapsulatedFeatures, CRA))
+	public static void main(String[] args) {
+		new CRAV2().run();
+	}
+
+	private void run() {
+		var store = ModelStore.builder()
+				.symbols(name, attribute, method, class_, functionalDependency, dataDependency, isEncapsulatedBy)
+				.with(QueryInterpreterAdapter.builder())
+				.with(ModelVisualizerAdapter.builder()
+						.withOutputPath("test_output")
+						.withFormat(FileFormat.DOT)
+						//.withFormat(FileFormat.SVG)
+						.saveStates()
+						.saveDesignSpace())
+				.with(StateCoderAdapter.builder())
+				.with(ModificationAdapter.builder())
+				.with(DesignSpaceExplorationAdapter.builder()
+						.transformations(deleteClass, createClass, moveFeature, assignFeature)
+						.objectives(Objectives.value(CRA))
+						.accept(Criteria.whenNoMatch(nonEncapsulatedFeatures)))
 				.build();
-	}
 
-	public Symbol<String> getName(){
-		return name;
-	}
-	public Symbol<Boolean> getAttribute(){
-		return attribute;
-	}
+		var model = store.createEmptyModel();
+		var queryEngine = model.getAdapter(ModelQueryAdapter.class);
 
-	public Symbol<Boolean> getMethod(){
-		return method;
-	}
+		var nameInterpretation = model.getInterpretation(name);
+		var attributeInterpretation = model.getInterpretation(attribute);
+		var methodInterpretation = model.getInterpretation(method);
+		var classInterpretation = model.getInterpretation(class_);
 
-	public Symbol<Boolean> getClass_(){
-		return class_;
-	}
+		var functionalDependencyInterpretation = model.getInterpretation(functionalDependency);
+		var dataDependencyInterpretation = model.getInterpretation(dataDependency);
+		var isEncapsulatedByInterpretation = model.getInterpretation(isEncapsulatedBy);
 
-	public Symbol<Integer> getNextID(){
-		return nextID;
-	}
+		var modificationAdapter = model.getAdapter(ModificationAdapter.class);
 
-	public Symbol<Boolean> getFunctionalDependency() {
-		return functionalDependency;
-	}
+		// Add all Methods
+		var method1 = modificationAdapter.createObject();
+		var method1Id = method1.get(0);
+		var method2 = modificationAdapter.createObject();
+		var method2Id = method2.get(0);
+		var method3 = modificationAdapter.createObject();
+		var method3Id = method3.get(0);
+		var method4 = modificationAdapter.createObject();
+		var method4Id = method4.get(0);
+		var attribute1 = modificationAdapter.createObject();
+		var attribute1Id = attribute1.get(0);
+		var attribute2 = modificationAdapter.createObject();
+		var attribute2Id = attribute2.get(0);
+		var attribute3 = modificationAdapter.createObject();
+		var attribute3Id = attribute3.get(0);
+		var attribute4 = modificationAdapter.createObject();
+		var attribute4Id = attribute4.get(0);
+		var attribute5 = modificationAdapter.createObject();
+		var attribute5Id = attribute5.get(0);
 
-	public Symbol<Boolean> getDataDependency() {
-		return dataDependency;
-	}
+		nameInterpretation.put(method1, "M1");
+		nameInterpretation.put(method2, "M2");
+		nameInterpretation.put(method3, "M3");
+		nameInterpretation.put(method4, "M4");
+		nameInterpretation.put(attribute1, "A1");
+		nameInterpretation.put(attribute2, "A2");
+		nameInterpretation.put(attribute3, "A3");
+		nameInterpretation.put(attribute4, "A4");
+		nameInterpretation.put(attribute5, "A5");
 
-	public Symbol<Boolean> getIsEncapsulatedBy() {
-		return isEncapsulatedBy;
-	}
+		methodInterpretation.put(method1, true);
+		methodInterpretation.put(method2, true);
+		methodInterpretation.put(method3, true);
+		methodInterpretation.put(method4, true);
+		attributeInterpretation.put(attribute1, true);
+		attributeInterpretation.put(attribute2, true);
+		attributeInterpretation.put(attribute3, true);
+		attributeInterpretation.put(attribute4, true);
+		attributeInterpretation.put(attribute5, true);
 
+		dataDependencyInterpretation.put(Tuple.of(method1Id, attribute1Id), true);
+		dataDependencyInterpretation.put(Tuple.of(method1Id, attribute3Id), true);
+		dataDependencyInterpretation.put(Tuple.of(method2Id, attribute2Id), true);
+		dataDependencyInterpretation.put(Tuple.of(method3Id, attribute3Id), true);
+		dataDependencyInterpretation.put(Tuple.of(method3Id, attribute4Id), true);
+		dataDependencyInterpretation.put(Tuple.of(method4Id, attribute3Id), true);
+		dataDependencyInterpretation.put(Tuple.of(method4Id, attribute5Id), true);
 
-	public RelationalQuery getDeleteClassPrecondition() {
-		return deleteClassPrecondition;
-	}
+		functionalDependencyInterpretation.put(Tuple.of(method1Id, attribute3Id), true);
+		functionalDependencyInterpretation.put(Tuple.of(method1Id, attribute4Id), true);
+		functionalDependencyInterpretation.put(Tuple.of(method2Id, attribute1Id), true);
+		functionalDependencyInterpretation.put(Tuple.of(method3Id, attribute1Id), true);
+		functionalDependencyInterpretation.put(Tuple.of(method3Id, attribute4Id), true);
+		functionalDependencyInterpretation.put(Tuple.of(method4Id, attribute2Id), true);
 
-	public RelationalQuery getCreateClassPrecondition() {
-		return createClassPrecondition;
-	}
+		var initialVersion = model.commit();
+		queryEngine.flushChanges();
 
-	public RelationalQuery getMoveFeaturePrecondition() {
-		return moveFeaturePrecondition;
-	}
+		var bestFirst = new BestFirstStoreManager(store, 500);
+		bestFirst.startExploration(initialVersion);
+		var resultStore = bestFirst.getSolutionStore();
+		System.out.println("states size: " + resultStore.getSolutions().size());
+		//model.getAdapter(ModelVisualizerAdapter.class).visualize(bestFirst.getVisualizationStore());
 
-	public RelationalQuery getAssignFeaturePrecondition() {
-		return assignFeaturePrecondition;
-	}
-	public RelationalQuery getNonEncapsulatedFeatures() {
-		return nonEncapsulatedFeatures;
-	}
+		double lowest_score = Double.POSITIVE_INFINITY;
+		int lowest_index = 0;
+		int counter = 0;
+		for (var solution: resultStore.getSolutions()){
+			//System.out.println("Score: " + solution.objectiveValue().get(0));
+			if (solution.objectiveValue().get(0) < lowest_score){
+				lowest_score = solution.objectiveValue().get(0);
+				lowest_index = counter;
+			}
+			counter ++;
+		}
 
-	public FunctionalQuery<Double> getCRA() {
-		return CRA;
-	}
+		System.out.println("Best Score: " + lowest_score);
+		System.out.println("Best Index: " + lowest_index);
 
-	public CRAModel createEmptyModel() {
-		return new CRAModel(this, store.createEmptyModel());
+		model.restore(resultStore.getSolutions().get(lowest_index).version());
 	}
 }
